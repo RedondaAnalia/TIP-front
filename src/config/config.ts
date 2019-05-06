f13e6a2e93e40e95561d3d5a837c090832e7313b
Saba <sabaliauskaspablo@gmail.com>
2019-05-05 22:10:27 -0300

    heroku configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,4 @@
-export const URL_SERVICIOS = 'http://localhost:3000/';
+import { environment } from '../environments/environment';
+const apiEndpoint: String = environment.apiEndpoint;
+
+export const URL_SERVICIOS = apiEndpoint;
