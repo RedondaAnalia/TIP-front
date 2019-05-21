d3b0ee3d6721ec7fdc3f2ecab3200ad57dd4d608
RedondaAnalia <a.redonda89@gmail.com>
2019-05-21 19:07:19 -0300

    Mapa se centra en la ubicacion actual de donde se esta ejecutando

--- src/app/components/veterinaries-map/veterinaries-map.component.ts
+++ src/app/components/veterinaries-map/veterinaries-map.component.ts
@@ -6,12 +6,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VeterinariesMapComponent implements OnInit {
 
-  latitude = -28.68352;
-  longitude = -147.20785;
+  lat = -34.603418;
+  lng = -58.381592;
+  zoom = 15;
 
-  constructor() { }
+   // Geolocalizacion
+   options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  };
+
+  constructor() {
+    navigator.geolocation.getCurrentPosition(this.success, this.error, this.options);
+  }
 
   ngOnInit() {
+  }
+
+  private success = (pos) => {
+    const crd = pos.coords;
+    this.lat = crd.latitude;
+    this.lng = crd.longitude;
+    console.log(this.lat);
+    console.log(this.lng);
+  }
+
+
+  private error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
   }
 
 }
