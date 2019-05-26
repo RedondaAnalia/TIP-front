badd62d0d1dec7c1a2cb10bf589efc26a15e4882
Saba <sabaliauskaspablo@gmail.com>
2019-05-26 18:49:26 -0300

    added pdf button for medicals cards - added level for users

--- src/app/components/show-medical-cards/show-medical-cards.component.ts
+++ src/app/components/show-medical-cards/show-medical-cards.component.ts
@@ -4,6 +4,8 @@ import { PetService } from '../../services/pet-service';
 import { UserService } from '../../services/user.service';
 import { MatDialog } from '@angular/material';
 import { NewMedicalCardComponent } from '../new-medical-card/new-medical-card.component';
+import * as jsPDF from 'jspdf';
+import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-show-medical-cards',
@@ -19,6 +21,22 @@ export class ShowMedicalCardsComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+
+  downloadPDF() {
+
+    html2canvas(document.getElementById('medical-content'), {
+      // Opciones
+      allowTaint: true,
+      useCORS: false,
+      // Calidad del PDF
+      scale: 1
+    }).then(function(canvas) {
+      const img = canvas.toDataURL('image/png');
+      const doc = new jsPDF('p', 'mm', 'A4');
+      doc.addImage(img, 'PNG', 7, 20);
+      doc.save('medicalPDF.pdf');
+    });
   }
 
   openDialog() {
